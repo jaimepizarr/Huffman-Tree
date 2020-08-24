f0daeb305fd3de0a5b989665718c0eca2ca7d73c
Claudia Asanza <clasrome@espol.edu.ec>
2020-08-24 01:32:10 -0500

    TDA arbol

--- Huffman/src/TDA/HuffmanTree.java
+++ Huffman/src/TDA/HuffmanTree.java
@@ -5,10 +5,65 @@
  */
 package TDA;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+
 /**
  *
  * @author PC
  */
 public class HuffmanTree {
+      private Node root;
     
+    private class Node{
+        private String data;
+        private Integer fq;
+        private Node left;
+        private Node right;
+
+        public Node(String data, Integer fq) {
+            this.data = data;
+            this.fq = fq;
+        }
+    }
+        
+        public void calcularArbol(HashMap<String, Integer> mapa){
+            PriorityQueue<Node> colaFq = new PriorityQueue<>((Node nd1, Node nd2) -> nd1.fq-nd2.fq);
+            for(Entry<String, Integer> e  : mapa.entrySet()){
+                colaFq.offer(new Node(e.getKey(), e.getValue()));
+            }
+            while(colaFq.size()>1){
+                Node nd1 = colaFq.poll();
+                Node nd2 = colaFq.poll();
+                Node padre = new Node(nd1.data+nd2.data, nd1.fq+nd2.fq);
+                padre.left = nd1;
+                padre.right = nd2;
+                colaFq.offer(padre);
+        
+            }
+            root = colaFq.poll();        
+        }
+        
+        
+        public HashMap<String, String> calcularCodigos(){
+            return null;
+        
+       
+        }
+        
+        
+        public static String codificar(String texto, HashMap<String,String> mapa){
+          return null;
+        
+        
+        
+        
+        }
+        public static String decodificar (String texto, HashMap<String,String> mapa){
+          return null;
+    
+    
+    }
+        
 }
