10c24703e6bdf77b011ace67d91683faf3806138
Joseph <josdavil@espol.edu.ec>
2020-08-24 16:17:53 -0500

    update chooseFile

--- Huffman/src/resources/fxml/MainController.java
+++ Huffman/src/resources/fxml/MainController.java
@@ -5,9 +5,15 @@
  */
 package resources.fxml;
 
+import java.awt.event.ActionEvent;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 /**
  * FXML Controller class
  *
@@ -18,9 +24,23 @@ public class MainController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private Button btnDescomprimir;
+    @FXML
+    private Button btnComprimir;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void singleFileChooser(javafx.event.ActionEvent event) {
+            FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(new ExtensionFilter("TXT files (.txt)", ".txt"));
+        File f= fc.showOpenDialog(null);
+        if(f!=null){
+        System.out.println(f.getAbsolutePath());}
+    }
     
 }
